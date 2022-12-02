48b5cbdc33481e3c9085dd631b6e9b047837c286
shaolang <shaolang@ymail.com>
2022-12-02 20:42:18 +0800

    Complete day 1 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,22 +1,29 @@
+use std::collections::BTreeSet;
 use std::fs;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     let f = fs::File::open("calories-input.txt").unwrap();
     let reader = BufReader::new(f);
-    let mut most: u64 = 0;
+    let mut tops = BTreeSet::<u64>::new();
     let mut curr: u64 = 0;
 
     for line in reader.lines() {
         let line = line.unwrap();
 
         if line.trim() == "" {
-            most = if most > curr { most } else { curr };
+            tops.insert(curr);
+
+            if tops.len() > 3 {
+                tops = tops.into_iter().skip(1).collect();
+            }
+
             curr = 0;
         } else {
             curr += line.parse::<u64>().unwrap();
         }
     }
 
-    println!("Most calories {}.", most);
+    println!("            Most calories: {}", tops.iter().last().unwrap());
+    println!("Total calories from top 3: {}", tops.into_iter().sum::<u64>());
 }
