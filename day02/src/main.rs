7ffcbf7e382621961c01375c67b5c791d5746804
shaolang <shaolang@ymail.com>
2022-12-02 22:29:20 +0800

    Complete day 2 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -2,22 +2,55 @@ use std::fs;
 use std::io::{BufRead, BufReader};
 
 fn main() {
+    let score_xyz_as_shapes = cal_score_from_guide(line_to_shapes_xyz_shapes);
+    println!("Total score (XYZ as shapes): {}", score_xyz_as_shapes);
+
+    let score_xyz_as_outcomes = cal_score_from_guide(line_to_shapes_xyz_outcomes);
+    println!("Total score (XYZ as outcomes): {}", score_xyz_as_outcomes);
+}
+
+fn cal_score_from_guide<F>(func: F) -> u16
+where
+    F: Fn(&str) -> Vec<Shape>,
+{
     let f = fs::File::open("strategy-guide.txt").unwrap();
     let reader = BufReader::new(f);
-    let total_score = reader
+
+    reader
         .lines()
         .map(|line| line.expect("Problem reading file"))
         .map(|line| {
-            let shapes = line_to_shapes(&line);
+            let shapes = func(&line);
             shapes.last().unwrap().score(*shapes.first().unwrap())
         })
-        .sum::<u16>();
-
-    println!("Total score: {}", total_score);
+        .sum()
 }
 
-fn line_to_shapes(line: &str) -> Vec<Shape> {
+fn line_to_shapes_xyz_shapes(line: &str) -> Vec<Shape> {
     line.split(' ').map(Shape::from_string).collect()
+}
+
+fn line_to_shapes_xyz_outcomes(line: &str) -> Vec<Shape> {
+    let ss = line
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+    let theirs = Shape::from_string(ss.first().unwrap());
+    let mine = match ss.last().unwrap().as_str() {
+        "X" => match theirs {
+            Shape::Rock => Shape::Scissors,
+            Shape::Paper => Shape::Rock,
+            _ => Shape::Paper,
+        },
+        "Y" => theirs,
+        _ => match theirs {
+            Shape::Rock => Shape::Paper,
+            Shape::Paper => Shape::Scissors,
+            _ => Shape::Rock,
+        },
+    };
+
+    vec![theirs, mine]
 }
 
 #[derive(Copy, Clone, PartialEq)]
